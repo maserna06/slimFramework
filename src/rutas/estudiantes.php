c88502067a9c07f0f9d7bb86956b8720252d742f
maserna06 <maserna06@gmail.com>
2021-03-15 14:25:38 -0500

    Api funcionando

--- src/rutas/estudiantes.php
+++ src/rutas/estudiantes.php
@@ -26,8 +26,8 @@ $app->get('/api/estudiantes', function(Request $request, Response $response) {
 });
 
 // GET Obtener informacion de un estudiante con su id
-$app->get('/api/estudiantes/{id}', function(Request $request, Response $response) {
-  $id = $request->getAttribute('id');
+$app->get('/api/estudiantes/{id}', function(Request $request, Response $response, array $args) {
+  $id = $args['id'];
 
   $sql = "SELECT * FROM estudiantes WHERE id = $id";
   try {
@@ -75,6 +75,72 @@ $app->post('/api/estudiantes/nuevo', function(Request $request, Response $respon
     $res->execute();
 
     echo 'Nuevo estudiantes creado';
+
+    $res = null;
+    $bd = null;
+  } catch(PDOException $e) {
+    echo '{"Error" : {"Text" : '.$e->getMessage().'}}';
+  }
+
+  return $response;
+});
+
+// PUT modificar un estudiante
+$app->put('/api/estudiantes/modificar/{id}', function(Request $request, Response $response, array $args) {
+  $id = $args['id'];
+  $nombres = $request->getParsedBody()['nombres'];
+  $apellidos = $request->getParsedBody()['apellidos'];
+  $direccion = $request->getParsedBody()['direccion'];
+  $celular = $request->getParsedBody()['celular'];
+  $email = $request->getParsedBody()['email'];
+
+  $sql = "UPDATE estudiantes SET
+            nombres = :nombres,
+            apellidos = :apellidos,
+            direccion = :direccion,
+            celular = :celular,
+            email = :email
+          WHERE id  = $id";
+  try {
+    $bd = new BD();
+    $bd = $bd->coneccionBD();
+    $res = $bd->prepare($sql);
+
+    $res->bindParam(':nombres', $nombres);
+    $res->bindParam(':apellidos', $apellidos);
+    $res->bindParam(':direccion', $direccion);
+    $res->bindParam(':celular', $celular);
+    $res->bindParam(':email', $email);
+
+    $res->execute();
+
+    echo 'Estudiante modificado';
+
+    $res = null;
+    $bd = null;
+  } catch(PDOException $e) {
+    echo '{"Error" : {"Text" : '.$e->getMessage().'}}';
+  }
+
+  return $response;
+});
+
+// DELETE modificar un estudiante
+$app->delete('/api/estudiantes/eliminar/{id}', function(Request $request, Response $response, array $args) {
+  $id = $args['id'];
+
+  $sql = "DELETE FROM estudiantes WHERE id  = $id";
+  try {
+    $bd = new BD();
+    $bd = $bd->coneccionBD();
+    $res = $bd->prepare($sql);
+    $res->execute();
+
+    if($res->rowcount() > 0) {
+      echo 'Estudiante eliminado';
+    } else {
+      echo 'No existe Estudiante con el id: ' . $id;
+    }
 
     $res = null;
     $bd = null;
